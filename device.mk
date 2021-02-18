a51a9e052bd54fa7323048b365756b8cc828be37
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-18 12:36:35 +0530

    RMX1831: Build Keymaster 3.0 HAL
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -180,6 +180,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
     android.hardware.health@2.0-service.override
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
