aae5cac21b5d1a76e03bf0b940e40a729da0b0e2
SparXFusion <s2234nadar@gmail.com>
2021-05-20 22:02:50 +0530

    RMX1831: initial tree
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

---
+++ device.mk
@@ -0,0 +1,80 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# Audio policy configuration
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 28
+PRODUCT_EXTRA_VNDK_VERSIONS := 28
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.manager-V1.0-java \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system \
+
+# WiFi
+PRODUCT_PACKAGES += \
+    hostapd \
+    libwpa_client \
+    libwifi-hal \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    WifiOverlay
+
+# KPOC
+PRODUCT_PACKAGES += \
+    libsuspend
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-service.override
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-service \
+    android.hardware.vibrator@1.0-impl
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+
+#RCS
+PRODUCT_PACKAGES += \
+    RcsService
+
+$(call inherit-product, vendor/realme/RMX1831/RMX1831-vendor.mk)   
+
