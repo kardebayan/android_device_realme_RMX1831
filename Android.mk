Radeon790/device_realme_RMX1831-1

RMX1831: Add blobs symlinks
- Based over https://github.com/GS290-dev/android_device_gigaset_GS290/commit/348b6ac634c5f2f2df2fd9ab27703353f62e9df5

Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
Signed-off-by: Debayan <kardebayan3@gmail.com>
Signed-off-by: SparXFusion <s2234nadar@gmail.com>

Android.mk
@@ -23,4 +23,51 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX1831)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6771.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6771_17065.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6771.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6771_17065.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6771.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.oppo6771_17065.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6771.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.oppo6771_17065.so
+$(KEYSTORE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcTeeKeymaster.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_SYMLINK)
+
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6771.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6771.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf ../egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
+
 endif

kardebayan/android_device_realme_RMX1831

Android.mk
@@ -23,4 +23,51 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX1831)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6771.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6771_18611.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6771.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6771_18611.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6771.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.oppo6771_18611.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6771.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.oppo6771_18611.so
+$(KEYSTORE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcTeeKeymaster.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_SYMLINK)
+
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6771.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6771.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf ../egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
+
 endif