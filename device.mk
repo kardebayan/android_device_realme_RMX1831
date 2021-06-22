8d2498b31d4c630f9903a5778769ac02a609ed25
TheMalachite <eliasgheeraert@gmail.com>
2021-06-22 12:08:12 +0000

    RMX1831: Build TinyXML
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -216,7 +216,11 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@1.0-service
+
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
 
 # RCS
 PRODUCT_PACKAGES += \
