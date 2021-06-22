86bcd1459d88cf74d07e1bfae3b88ff44d2437bc
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-06-22 11:57:21 +0000

    RMX1831: Build libsensorndkbridge from source
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Sensors
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
